5889358c1eafb618e498dba377dd717cfb0fbd9f
PiotrekB416 <89194651+PiotrekB416@users.noreply.github.com>
2022-05-25 19:46:36 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/App.jsx";
+import {App, Opinion} from "./app/App.jsx";
 import Sub_Menu from "./Button/LoadButton";
 import Change from "./Roll/Change";
-const root = [createRoot(document.getElementById('tytul'))];
-root[0].render(App())
 
+const root = [createRoot(document.getElementById('tytul')), createRoot(document.getElementById('footer'))];
+root[0].render(App())
+root[1].render(Opinion())
 
 Sub_Menu(document);
 
